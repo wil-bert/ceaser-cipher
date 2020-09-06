faef603b477ee54f386de8f54f1d68993794846c
wil-bert <ianwilbuts@gmail.com>
2020-09-06 16:27:32 +0300

    Edit encoding class

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,23 +1,49 @@
 public class Encoding {
-    private int mKey;
+    private  int mKey;
     private String mInputText;
 
     public Encoding(String inputText, int Key) {
         mKey = Key;
         mInputText = inputText;
     }
-
-    public String getInputText() {
-        return mInputText;
+    public String getInputText(){
+        return  mInputText;
     }
-
     public int getKey() {
         return mKey;
     }
-}
-//    public boolean isValidInputText() {
-//        char[] chars = mInputText.toCharArray();
-//
-//        for (char c : chars) {
-//    }
-//}
+    public boolean isValidInputText() {
+        char[] chars = mInputText.toCharArray();
+
+        for (char c : chars) {
+            if (!Character.isLetter(c) && c != ' ') {
+                return  false;
+            }
+        }
+        return  true;
+    }
+
+    public boolean isNotEmpty() {
+        return  !mInputText.trim().isEmpty();
+    }
+    public boolean isValidKey(){
+        return mKey >1 && mKey <26;
+    }
+
+    public  System encode(){
+        String encoded = "";
+        String encodedArray[] = mInputText.split("");
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i<encodedArray.length;i++) {
+            if(mInputText.charAt(i) == ' '){
+                encoded += " ";
+            }
+            else {
+                int charPosition = alphabet.indexOf(mInputText.charAt(i));
+                int Key = (mKey + charPosition) % 26;
+                encoded += alphabet.charAt(Key);
+            }
+        }
+        return encoded.toUpperCase();
+    }
+}}
