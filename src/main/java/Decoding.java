24660e0c02cd18f7e152605c829d18fc3527ecb9
wil-bert <ianwilbuts@gmail.com>
2020-09-06 18:41:18 +0300

    Edit decryption class

--- src/main/java/Decoding.java
+++ src/main/java/Decoding.java
@@ -13,4 +13,41 @@ public class Decoding {
     public int getKey() {
         return mKey;
     }
+    public boolean isValidInputText() {
+        char[] chars = mInputText.toCharArray();
+
+        for (char c : chars) {
+            if (!Character.isLetter(c) &&  c != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean isValidKey(){
+        return mKey >1 && mKey <26;
+    }
+
+    public String decode(){
+        String decodedValue = "";
+        String encodedArray[] = mInputText.split("");
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i<encodedArray.length;i++){
+            if (mInputText.charAt(i) == ' '){
+                decodedValue += " ";
+            }
+            else {
+                int charPosition = alphabet.indexOf(mInputText.charAt(i));
+                int keyVal = (charPosition - mKey) % 26;
+
+                if (keyVal < 0) {
+                    keyVal = alphabet.length() + keyVal;
+                }
+
+                char replaceValue = alphabet.charAt(keyVal);
+                decodedValue += replaceValue;
+            }
+        }
+        return decodedValue;
+    }
+
 }
